5f337fb65bed4f19b9042549796caa6df8d0aa62
Mainm0e <mainm0e1996@gmail.com>
2023-09-25 12:49:15 +0300

    update page

--- app/about_me/page.tsx
+++ app/about_me/page.tsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
 
   useEffect(() => {
     // Fetch the JSON data from the file (adjust the path as needed)
-    fetch('../API/skills/skills.json')
+    fetch('./data/skills.json')
       .then((response) => response.json())
       .then((jsonData) => {
         // Set the data in state
@@ -26,17 +26,6 @@ export default function AboutMe() {
       <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
         <h1 className="text-3xl font-semibold text-gray-800 mb-6">About Me</h1>
         <div className="flex flex-col md:flex-row">
-          {/* Backend Skills (Left Side) */}
-          <div className="md:w-1/2 pr-4">
-            <h2 className="text-xl font-semibold text-gray-700 mb-4">Backend Skills</h2>
-            <ul className="list-disc pl-4">
-              {data.backSkills.map((skill, index) => (
-                <li key={index} className="text-gray-600 mb-2">
-                  {skill.skill} - {skill.experience}
-                </li>
-              ))}
-            </ul>
-          </div>
 
           {/* Frontend Skills (Right Side) */}
           <div className="md:w-1/2 pl-4">
@@ -49,6 +38,19 @@ export default function AboutMe() {
               ))}
             </ul>
           </div>
+
+           {/* Backend Skills (Left Side) */}
+           <div className="md:w-1/2 pr-4">
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">Backend Skills</h2>
+            <ul className="list-disc pl-4">
+              {data.backSkills.map((skill, index) => (
+                <li key={index} className="text-gray-600 mb-2">
+                  {skill.skill} - {skill.experience}
+                </li>
+              ))}
+            </ul>
+          </div>
+
         </div>
       </div>
     </div>
