0eb4682f015e04dc3438e1e896d48194c948fa1e
Mainm0e <mainm0e1996@gmail.com>
2023-09-25 19:06:20 +0300

    update loading effect to about skill component

--- app/about/AboutSkill.tsx
+++ app/about/AboutSkill.tsx
@@ -12,32 +12,45 @@ interface SkillData {
   backSkills: Skill[];
 }
 
-
 export default function AboutSkill() {
-    const [data, setData] = useState<SkillData>({
-        frontendSkills: [],
-        backSkills: [],
-      });
-    
-      useEffect(() => {
-        // Fetch the JSON data from the file
-        fetch("./data/skills.json")
-          .then((response) => response.json())
-          .then((jsonData: SkillData) => {
-            // Set the data in state
-            setData(jsonData);
-          })
-          .catch((error) => {
-            console.error("Error loading JSON data:", error);
-          });
-      }, []);
+  const [data, setData] = useState<SkillData>({
+    frontendSkills: [],
+    backSkills: [],
+  });
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
 
-    return (
-        <div className="max-w-4xl mx-auto bg-primary p-6 rounded-lg shadow-lg">
-        <div className="flex flex-col md:flex-row ">
+  useEffect(() => {
+    // Fetch the JSON data from the file
+    fetch("./data/skills.json")
+      .then((response) => response.json())
+      .then((jsonData: SkillData) => {
+        // Set the data in state
+        setData(jsonData);
+        // Turn off loading state
+        /* add 1s delay */
+        setTimeout(() => {
+        setIsLoading(false);
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("Error loading JSON data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="max-w-4xl mx-auto bg-primary p-6 rounded-lg shadow-lg">
+      {isLoading ? ( // Check if data is loading
+        // Display loading animation here
+        <div className="flex flex-row items-center justify-center">
+         <span className="loading loading-dots loading-lg"></span>
+        </div>
+      ) : (
+        <div className="flex flex-col md:flex-row">
           {/* Frontend Skills (Right Side) */}
-          <div className="md:w-1/2 md:pr-4 flex flex-col items-center md:border-r-2 ">
-            <h2 className="text-xl font-semibold text-gray-700 mb-4">Frontend  <i className="fa-solid fa-laptop-code fa-flip fa-gl"></i></h2>
+          <div className="md:w-1/2 md:pr-4 flex flex-col items-center md:border-r-2">
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">
+              Frontend <i className="fa-solid fa-laptop-code fa-flip fa-gl"></i>
+            </h2>
             <ul className="list-none pl-6">
               {data.frontendSkills.map((skill, index) => (
                 <li key={index} className="text-gray-600 mb-2">
@@ -48,8 +61,9 @@ export default function AboutSkill() {
           </div>
           {/* Backend Skills (Left Side) */}
           <div className="md:w-1/2 md:pr-4 flex flex-col items-center max-sm:border-t-2 max-sm:pt-5">
-          
-            <h2 className="text-xl font-semibold text-gray-700 mb-4">Backend  <i className="fa-solid fa-gears fa-beat fa-gl"></i></h2>
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">
+              Backend <i className="fa-solid fa-gears fa-beat fa-gl"></i>
+            </h2>
             <ul className="list-none pl-6">
               {data.backSkills.map((skill, index) => (
                 <li key={index} className="text-gray-600 mb-2">
@@ -59,7 +73,7 @@ export default function AboutSkill() {
             </ul>
           </div>
         </div>
-      </div>
-      
-    );
+      )}
+    </div>
+  );
 }
--- app/footer.tsx
+++ app/footer.tsx
@@ -9,25 +9,15 @@ export default function Footer() {
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
           <div className="px-5 py-2">
             <Link
-              href="/about_me"
+              href="/about"
               className="text-base leading-6 text-gray-500 hover:text-gray-900"
             >
-              About Me
+              About
             </Link>
           </div>
-          {/* <div className="px-5 py-2">
-                        <Link href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                            Blog
-                        </Link>
-                    </div> */}
-          {/* <div className="px-5 py-2">
-                        <Link href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                            Pricing
-                        </Link>
-                    </div> */}
           <div className="px-5 py-2">
             <Link
-              href="#"
+              href="/contact"
               className="text-base leading-6 text-gray-500 hover:text-gray-900"
             >
               Contact
@@ -43,12 +33,6 @@ export default function Footer() {
           </div>
         </nav>
         <div className="flex justify-center mt-8 space-x-6">
-          {/* <Link href="#" className="text-gray-400 hover:text-gray-500">
-                        <span className="sr-only">Facebook</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                            <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd"></path>
-                        </svg>
-                    </Link> */}
           <Link
             href="https://www.instagram.com/mainm0e/"
             className="text-gray-400 hover:text-gray-500"
